6019ea8868509cf4a8f3b173faa658f1c9f00716
Liujunyi <liujunyi671@163.com>
2024-06-19 19:01:50 +0800

    first commit

---
+++ train_iu_xray.sh
@@ -0,0 +1,23 @@
+python main_train.py\
+    --image_dir /root/R2G/data/iu_xray_data/images/ \
+    --ann_path /root/R2G/data/iu_xray_data/annotation.json \
+    --dataset_name iu_xray \
+    --max_seq_length 60 \
+    --threshold 3 \
+    --epochs 70 \
+    --batch_size 16 \
+    --lr_ve 2e-4 \
+    --lr_ed 5e-4 \
+    --step_size 10 \
+    --gamma 0.8 \
+    --num_layers 3 \
+    --topk 32 \
+    --cmm_size 2048 \
+    --cmm_dim 512 \
+    --seed 7580 \
+    --beam_size 3 \
+    --save_dir results/iu_xray0823_bs8_layer5_CMN/ \
+    --log_period 50 \
+    --early_stop 40 \
+    --device 1 \
+    > log/6.11/lr_ve_2e_4_lr_ed_5e_4_base_line_transformer_bs16_device_1_iu_xray.log 2>&1 &
